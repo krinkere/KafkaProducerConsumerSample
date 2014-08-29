5dfbc3cad414ac64ff69c567d25b3aea9f3ae27e
Al Krinker <alkrinker@C02M24NZFD59.local>
2014-08-29 11:47:20 -0400

    Added comments and Got KafkaConsumer to work

--- src/main/java/com/mj/KafkaProducer.java
+++ src/main/java/com/mj/KafkaProducer.java
@@ -1,7 +1,5 @@
 package com.mj;
 
-
-
 import java.util.Properties;
 
 import kafka.javaapi.producer.Producer;
@@ -11,39 +9,51 @@ import kafka.producer.ProducerConfig;
 public class KafkaProducer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		Properties props = new Properties();
 		 
-		props.put("metadata.broker.list", "host2:9092,host3:9092");
+                // List of brokers that the producer will try to connect to
+                props.put("metadata.broker.list", "192.168.33.21:9092,192.168.33.22:9092");
+                // Since we are sending text messages, use StringEncoder
 		props.put("serializer.class", "kafka.serializer.StringEncoder");
-		// props.put("partitioner.class", "example.producer.SimplePartitioner");
+		// Consider publish message to be completed when you receive an acknoledgement from the leader
 		props.put("request.required.acks", "1");
 		 
 		ProducerConfig config = new ProducerConfig(props);
 		
+                // Create Producer that will be sending messages to Kafka brokers
 		Producer<String, String> producer = new Producer<String, String>(config);
 		
-		String date = "04092014" ;
-		// String topic = "my-replicated-topic" ;
-		String topic = "mytopic" ;
+		// Get a list of topics by running following command
+                // $KAFKA_HOME/bin/kafka-list-topic.sh --zookeeper 192.168.33.21:2181
+//                [2014-08-29 15:31:30,208] INFO zookeeper state changed (SyncConnected) (org.I0Itec.zkclient.ZkClient)
+//                topic: korb_test	partition: 0	leader: 1	replicas: 1	isr: 1
+//                topic: my_test_topic	partition: 0	leader: 2	replicas: 2	isr: 2
+//                topic: topic-1	partition: 0	leader: 1	replicas: 1	isr: 1
+                // if no topics exist, below code will create one for you
+                String topic = "my_test_topic" ;		
 		
-		
-		for (int i = 1 ; i <= 1000 ; i++) {
+		for (int i = 1 ; i <= 10 ; i++) {
 			
-			String msg = date + " This is message " + i ;
-			System.out.println(msg) ;
-			
-			KeyedMessage<String, String> data = new KeyedMessage<String, String>(topic, String.valueOf(i), msg);
-			 
-			producer.send(data);
-			
-			
+                    String msg = "Test message # " + i ;
+                    System.out.println("About to send '" + msg + "' message to Kafka!") ;
+
+                    KeyedMessage<String, String> data = new KeyedMessage<String, String>(topic, String.valueOf(i), msg);
+                    producer.send(data);
 		}
-		
+		// Use following command to verify that messages were received
+                // vagrant@debian-70rc1-x64-vbox4210:~$ sudo $KAFKA_HOME/bin/kafka-console-consumer.sh --zookeeper 192.168.33.21:2181 --topic my_test_topic  --from-beginning
+                // You should see something like this
+//                [2014-08-29 15:28:39,734] INFO [ConsumerFetcherManager-1409326119149] Adding fetcher for partition [my_test_topic,0], initOffset -1 to broker 2 with fetcherId 0 (kafka.consumer.ConsumerFetcherManager)
+//                Test message # 1
+//                Test message # 2
+//                Test message # 3
+//                Test message # 4
+//                Test message # 5
+//                Test message # 6
+//                Test message # 7
+//                Test message # 8
+//                Test message # 9
+//                Test message # 10
 		producer.close();
-		
-
 	}
-
 }
